79ceb4e4f340756038d239a70763435bcb036e08
Cedric Adjih <cedric.adjih@inria.fr>
2012-10-07 10:07:55 +0200

    added pir application w/ i2c workaround

---
+++ hipercom/pir/project-conf.h
@@ -0,0 +1,17 @@
+/*---------------------------------------------------------------------------
+ *                     Sniffer Project Configuration
+ *        Cedric Adjih, Hipercom Project-Team, Inria Paris-Rocquencourt
+ *                       Copyright 2012 Inria. 
+ *            All rights reserved. Distributed only with permission.
+ *---------------------------------------------------------------------------*/
+
+#ifndef DEFAULT_CHANNEL
+#define DEFAULT_CHANNEL 26
+#endif
+
+/*---------------------------------------------------------------------------*/
+
+/* Disable I2C init for using PIR */
+#define WITHOUT_I2C_INIT
+
+/*---------------------------------------------------------------------------*/
